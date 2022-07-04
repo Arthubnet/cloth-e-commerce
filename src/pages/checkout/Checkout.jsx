52527df9fcd2c3f0eafa69391b5b03d9f3175f99
Lena <z.otig@yahoo.com>
2022-07-03 21:05:35 -0600

    stripe description

--- src/pages/checkout/Checkout.jsx
+++ src/pages/checkout/Checkout.jsx
@@ -37,7 +37,7 @@ const CheckoutPage = ({ cartItems, total }) => (
     </div>
     <div className="test-warning">
       'Use the following <span>test</span> credit card for payments' <br /> 4242
-      4242 4242 4242 - Exp: 01/20 - CVV: 123
+      4242 4242 4242 - Exp: 01/23(as long as not expired) - CVV: 123(any)
     </div>
     <StripeCheckoutButton price={total} />
   </div>
