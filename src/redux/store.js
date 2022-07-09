2bdbef65ffa8ca3b2a13e625e2d9ecec82b71c00
Lena <z.otig@yahoo.com>
2022-07-08 21:06:00 -0600

    removed tools

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,15 +1,11 @@
-import { createStore, applyMiddleware } from "redux";
-import logger from "redux-logger"; //for debuggin, we can console errors
+import { createStore } from "redux";
+/* import logger from "redux-logger"; */ //for debuggin, we can console errors
 import rootReduser from "./root-reduser";
-import { composeWithDevTools } from "redux-devtools-extension";
 import { persistStore } from "redux-persist";
 
-const middlewares = [logger];
+/* const middlewares = [logger]; */
 
-const store = createStore(
-  rootReduser,
-  composeWithDevTools(applyMiddleware(...middlewares))
-);
+const store = createStore(rootReduser);
 
 const persistor = persistStore(store);
 
