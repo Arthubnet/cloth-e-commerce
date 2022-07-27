16ea162f88004ecb60bf81a6dd7ffb05d9cdc842
Lena <z.otig@yahoo.com>
2022-07-27 15:42:01 -0600

    hero img

--- src/pages/homepage/Hero.jsx
+++ src/pages/homepage/Hero.jsx
@@ -1,11 +1,12 @@
 import React from "react";
 import "./hero.styles.scss";
 import { Link } from "react-router-dom";
+import heroImg from "../../assets/sweatshirt.jpg";
 function Hero() {
   return (
     <div className="hero">
       <div className="hero__img">
-        <img src="https://i.ibb.co/xXZgR0s/hero1.jpg" alt="hero_image" />
+        <img src={heroImg} alt="hero_image" />
       </div>
       <div className="hero__title">
         <h1>A British Icon</h1>
